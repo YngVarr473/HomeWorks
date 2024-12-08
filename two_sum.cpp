8ac19e972b3ef1b5fb79a7c18482f407033b4328
YngVarr473 <igorkazakov682@gmail.com>
2024-12-08 17:39:29 +0500

    Update two_sum.cpp

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,25 +1,21 @@
-#include <iostream>
-#include <vector>
-
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> result();
+        vector<int> result{};
         
         for (int i = 0; i < nums.size(); i++) {
-             if (i - 1 >= 0) {
+            if (i - 1 >= 0) {
 
-            if (nums[i - 1] > 0 && nums[i] > 0) {
-                if (nums[i - 1] + nums[i] == target) {
-                    result.push_back(nums[i - 1], nums[i])
+                if (nums[i - 1] > 0 && nums[i] > 0) {
+                    if (nums[i - 1] + nums[i] == target) {
+                        result.push_back(nums[i - 1], nums[i])
+                    }
                 }
+            } else if (nums[i] + nums[i + 1] == target) {
+                    result.push_back(nums[i], nums[i + 1])
             }
-        } else if (nums[i] + nums[i + 1] == target) {
-                result.push_back(nums[i], nums[i + 1])
-            }
-
+        }
+    std::cout << result[i],[i+]
         return result;
     }
-}
-}
+};
