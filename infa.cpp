b76bdbdee8665e5434176aa48b0529f99bb7f542
YngVarr473 <igorkazakov682@gmail.com>
2024-12-16 14:48:15 +0500

    Create infa.cpp

---
+++ infa.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    double a = 2,7;
+    double c = 6;
+
+    double result = ((((4*(a**2)) - (6*a*c)) / ((2 * a) - (3 * c))) - ((((6*a*c) ** 0,5)) + (9 * (c**2))) / (((2 * a) + (3 * c)) ** 2)) * 
+    (((6*a) + 9) / ((4*a) + 9));
+
+std::cout << result;
+    return 0;
+}
