26eb368ec86d52729915242b935b6aab67facd23
YngVarr473 <igorkazakov682@gmail.com>
2024-12-18 18:02:16 +0500

    Update infa.cpp

--- infa.cpp
+++ infa.cpp
@@ -2,11 +2,13 @@
 #include <cmath>
 
 int main() {
-    double a = 2,7;
+    double a = 2.7;
     double c = 6;
 
-    double result = ((((4*(a**2)) - (6*a*c)) / ((2 * a) - (3 * c))) - ((((6*a*c) ** 0,5)) + (9 * (c**2))) / (((2 * a) + (3 * c)) ** 2)) * 
-    (((6*a) + 9) / ((4*a) + 9));
+double result = ((((4 * std::pow(a, 2)) - (6 * a * c)) / ((2 * a) - (3 * c))) 
+                - ((std::sqrt(6 * a * c) + (9 * std::pow(c, 2))) / std::pow((2 * a) + (3 * c), 2))) 
+                * ((6 * a + 9) / (4 * a + 9));
+
 
 std::cout << result;
     return 0;
