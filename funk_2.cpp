d63c98cb0ec23523b332d663beb2e19329e35651
YngVarr473 <igorkazakov682@gmail.com>
2025-01-26 17:40:49 +0500

    Update funk_2.cpp

--- funk_2.cpp
+++ funk_2.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
-#include <vector>
-#include <iomanip> 
 #include <cmath>
 
-//sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-
-bool IsPointInSquare(double x, double y) {
-    std::cin >> x >> y;
-    double dist = abs(x) <= 1 && abs(y <= 1);
-    return dist;
+int fib(int n) {
+    if (n == 1) {
+        return 1;
+    } else {
+        fib(n / 2);
+    }
 }
 
 int main() {
-    double x, y;
-    std::cin >> x >> y;
-    if (IsPointInSquare(x,y)) {
-        std::cout << "YES";
-    } else {
-        std::cout << "NO"
-    }
+    int n;
+    std::cin >> n;
+
+    std::cout << fib(n) << std::endl;
 }
