cba49334d778eb8e31f4177cf23ff189836fe7d8
YngVarr473 <igorkazakov682@gmail.com>
2025-02-09 17:34:55 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,20 @@
 import aiogram, os, asyncio
-from aiogram import *
+from aiogram import Bot, Dispatcher
 from aiogram.types import Message
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
+
 
 TOKEN ="7747265900:AAGW-tRZNz_Di8_efuf_Ren1wqdHf02rnv4"
 
-dp = Dispatcher(Bot)
+dp = Dispatcher()
 
 @dp.message(CommandStart())
-async def mess(message: Message):
+async def start(message: Message):
     await message.answer("Star")
+
+@dp.message(Command("help"))
+async def help(message: Message):
+    await message.answer("Do you need any help?")
 
 async def main():
     bot = Bot(token = TOKEN)
